5693d4c5d0323a264e38154e3b605ea5307e9623
ValeryManoilov <manojlovv@inbox.ru>
2024-11-20 22:17:31 +0300

    add: solution for 18th lesson

---
+++ Course/3rd year/Lesson18/Practice18/Practice18/ContactRepository.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Xml.Linq;
+using SQLitePCL;
+using System.Reflection.PortableExecutable;
+namespace Practice18
+{
+    public class ContactRepository
+    {
+        private readonly string _connectionString;
+
+        public ContactRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void CreateTable()
+        {
+            using (SqliteConnection connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = @"CREATE TABLE IF NOT EXISTS Contacts (
+                                   Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                   Name TEXT NOT NULL,
+                                   Email TEXT NOT NULL,
+                                   PhoneNumber TEXT NOT NULL
+                               )";
+
+                using (SqliteCommand command = new SqliteCommand(query, connection)) {
+                    command.ExecuteNonQuery();
+                }
+            }
+            Console.WriteLine("Таблица создана");
+        }
+
+        public void DeleteContact(string name)
+        {
+            using (SqliteConnection connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = @"DELETE FROM Contacts WHERE Name = @Name ";
+
+                using (SqliteCommand command = new SqliteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void AddContact(string name, string email, string phoneNumber)
+        {
+            using(SqliteConnection connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = @"INSERT INTO Contacts(Name, Email, PhoneNumber)
+                                 VALUES(@Name, @Email, @PhoneNumber)";
+
+                using (SqliteCommand command = new SqliteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void GetAllContacts()
+        {
+            using(SqliteConnection connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = @"SELECT * FROM Contacts";
+
+                using(SqliteCommand command = new SqliteCommand(query, connection))
+                {
+                    var data = command.ExecuteReader();
+
+                    while (data.Read())
+                    {
+                        Console.WriteLine(String.Format("{0} - {1} - {2} - {3}", data[0], data[1], data[2], data[3]));
+                    }
+                }
+            }
+        }
+
+        public bool GetContactByName(string name)
+        {
+            using (SqliteConnection connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = @"SELECT * FROM Contacts WHERE Name = @Name";
+
+                using (SqliteCommand command = new SqliteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", name);
+                    var data = command.ExecuteReader();
+
+                    if (data != null)
+                    {
+                        while (data.Read())
+                        {
+                            Console.WriteLine(String.Format("{0}-{1}-{2}-{3}", data[0], data[1], data[2], data[3]));
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Course/3rd year/Lesson18/Practice18/Practice18/Program.cs
@@ -0,0 +1,106 @@
+﻿using SQLitePCL;
+
+namespace Practice18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLitePCL.Batteries.Init();
+
+            string connectionString = "Data Source=data.db";
+
+            var Table = new ContactRepository(connectionString);
+
+            bool flag = true;
+
+            while (flag)
+            {
+                Console.WriteLine("Выберите функцию:");
+                Console.WriteLine("1) Посмотреть все контакты");
+                Console.WriteLine("2) Посмотреть контакт по имени");
+                Console.WriteLine("3) Добавить контакт");
+                Console.WriteLine("4) Удалить контакт по имени");
+                Console.WriteLine("5) Выход");
+
+                string ans = Console.ReadLine();
+
+                switch (ans)
+                {
+                    case ("1"):
+                        Console.WriteLine("---------------------------------");
+
+                        Table.GetAllContacts();
+
+                        Console.WriteLine("---------------------------------");
+                        break;
+
+                    case ("2"):
+                        Console.WriteLine("---------------------------------");
+
+                        Console.WriteLine("Введите имя");
+
+                        string contactName = Console.ReadLine();
+
+                        var needContact = Table.GetContactByName(contactName);
+
+                        if (!needContact)
+                        {
+                            Console.WriteLine("Такого контакта не найдено");
+                        }
+
+                        Console.WriteLine("---------------------------------");
+                        break;
+
+                    case ("3"):
+                        Console.WriteLine("---------------------------------");
+
+                        Console.WriteLine("Введите имя");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine("Введите почту");
+                        string email = Console.ReadLine();
+
+                        Console.WriteLine("Введите номер телефона");
+                        string phone = Console.ReadLine();
+
+                        Table.AddContact(name, email, phone);
+
+                        Console.WriteLine("Контакт добавлен");
+
+                        Console.WriteLine("---------------------------------");
+                        break;
+
+                    case ("4"):
+                        Console.WriteLine("---------------------------------");
+
+                        Console.WriteLine("Введите имя:");
+
+                        string contactDelName = Console.ReadLine();
+
+                        var deletingContact = Table.GetContactByName(contactDelName);
+
+                        if (!deletingContact)
+                        {
+                            Console.WriteLine("Такого контакта не найдено");
+                        }
+                        else
+                        {
+                            Table.DeleteContact(contactDelName);
+                        }
+
+                        Console.WriteLine("---------------------------------");
+                        break;
+
+                    case ("5"):
+                        Console.WriteLine("Спасибо, что пользуетесь нашим приложением!");
+                        flag = false;
+                        break;
+
+                    default:
+                        break;
+                    }
+                }
+        }
+    }
+}
